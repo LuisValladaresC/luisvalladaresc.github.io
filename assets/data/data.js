975e8f4a095674b2df7b39d3deded5f5e456b862
LuisValladaresC <ls.valladares7@gmail.com>
2020-05-07 14:56:10 -0600

    Implementacion de base de datos y carga dinamica

---
+++ assets/data/data.js
@@ -0,0 +1,131 @@
+const DATA = {
+    conocimientos: [
+        {
+            titulo: "HTML",
+            nivel: "90"
+        },
+        {
+            titulo: "CSS",
+            nivel: "85"
+        },
+        {
+            titulo: "Javascript",
+            nivel: "80"
+        },
+        {
+            titulo: "Bootstrap",
+            nivel: "80"
+        },
+        {
+            titulo: "Git & GitHub",
+            nivel: "85"
+        },
+    ],
+    servicios: [
+        {
+            titulo: "Maquetación de website",
+            descripcion: "Transformamos el diseño de un producto web en un conjunto de archivos capaces de ser reproducidos por los navegadores web.",
+            fecha_inicio: 2018,
+            imagen: {
+                url: './assets/img/public/trabajo-maquetacion.png',
+                url2x: './assets/img/public/trabajo-maquetacion@2x.png'
+            },
+            herramientas: ["html", "css", "bootstrap"]
+        },
+        {
+            titulo: "Interactividad local y remota",
+            descripcion: "Dotamos su pagina web con una interfaz dinamica e interactiva ayudandole a brindar una experiencia completa y memorable.",
+            fecha_inicio: 2018,
+            imagen: {
+                url: './assets/img/public/trabajo-interactividad.png',
+                url2x: './assets/img/public/trabajo-interactividad@2x.png'
+            },
+            herramientas: ["javascript"]
+        },
+        {
+            titulo: "Trabajo remoto y respaldos",
+            descripcion: "El desarrollo de su sitio web se realiza de manera remota con respaldos en la nube que garantizan la seguridad del proyecto.",
+            fecha_inicio: 2018,
+            imagen: {
+                url: './assets/img/public/trabajo-remoto.png',
+                url2x: './assets/img/public/trabajo-remoto@2x.png'
+            },
+            herramientas: ["git", "github"]
+        },
+    ],
+    proyectos: [
+        {
+            titulo: "Platzi Conf Hawaii",
+            resumen: "Sitio web desarrolado como modelo para promover un evento social.",
+            descripcion: "Platzi Conf Hawaii fue desarrolado como modelo para promover un evento social. Cuenta con una interface responsive de diseño atractivo lograda con ayuda de Bootstrap.",
+            url: {
+                website: 'https://luisvalladaresc.github.io/platzi-conf',
+                github: 'https://github.com/LuisValladaresC/platzi-conf'
+            },
+            imagen: {
+                portada: {
+                    url: "./assets/img/public/portafolio-platzi-conf.png",
+                    url2x: "./assets/img/public/portafolio-platzi-conf@x2.png",
+                    responsive: "./assets/img/public/portafolio-platzi-conf-stretch.png",
+                },
+                descripcion: {
+                    url: "./assets/img/public/modal-platzi-conf.png",
+                    url2x: "./assets/img/public/modal-platzi-conf@x2.png"
+                },
+            },
+            herramientas: ["html", "css", "bootstrap"]
+        },
+        {
+            titulo: "Platzi Video",
+            resumen: "Sitio web desarrolado como modelo de un servicio de peliculas online.",
+            descripcion: "Platzi Video fue creado como modelo para un servicio de películas online. Cuenta con un código que realiza multiples peticiones a API's externas y una interface responsive de diseño simple y pulido.",
+            url: {
+                website: 'https://luisvalladaresc.github.io/platzi-video',
+                github: 'https://github.com/LuisValladaresC/platzi-video'
+            },
+            imagen: {
+                portada: {
+                    url: "./assets/img/public/portafolio-platzi-video.png",
+                    url2x: "./assets/img/public/portafolio-platzi-video@x2.png",
+                    responsive: "./assets/img/public/portafolio-platzi-video-stretch.png"
+                },
+                descripcion: {
+                    url: "./assets/img/public/modal-platzi-video.png",
+                    url2x: "./assets/img/public/modal-platzi-video@x2.png"
+                },
+            },
+            herramientas: ["html", "css", "javascript"]
+        },
+        {
+            titulo: "Portafolio Personal",
+            resumen: "Sitio web actual desarrollado para promover mi marca personal.",
+            descripcion: "Sitio web actual desarrollado para promover mi marca personal. Cuenta con un sistema de mensajes de correo, multiples y muy bien pulidas animaciones y una interface responsive que resalta por su diseño simple, calido y atractivo.",
+            url: {
+                website: 'https://luisvalladaresc.github.io/portafolio',
+                github: ''
+            },
+            imagen: {
+                portada: {
+                    url: "./assets/img/public/portafolio-personal.png",
+                    url2x: "./assets/img/public/portafolio-personal@x2.png",
+                    responsive: "./assets/img/public/portafolio-personal-stretch.png"
+                },
+                descripcion: {
+                    url: "./assets/img/public/modal-portafolio.png",
+                    url2x: "./assets/img/public/modal-portafolio@x2.png"
+                },
+            },
+            herramientas: ["html", "css", "javascript"]
+        }
+    ],
+    contacto: {
+        acerca_de_mi: "Soy desarrollor de paginas web con un alto nivel de estudios en la rama. Mi trabajo es el fruto de detalle y pasión y justo ahora me encuentro explorando nuevas oportunidades para continuar creciendo profesionalmente.",
+        correo: "ls.valladares7@gmail.com",
+        redes_sociales: {
+            github: 'https://github.com/LuisValladaresC/',
+            linkedin: 'https://www.linkedin.com/in/valladaresc7/',
+            instagram: '',
+            twitter: ''
+        }
+    }
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,194 @@
+/* ---------------------------------------------------------------------------------- */
+/* CARGAMOS EL CONTENIDO DE LAS SECCIONES PROFESIONAL, TRABAJO, PORTAFOLIO Y CONTACTO */
+/* ---------------------------------------------------------------------------------- */
+
+const $contenedor_conocimientos = document.querySelector("#profesional .profesional-conocimientos");
+const $contenedor_servicios = document.querySelector("#trabajo .trabajo-servicios");
+const $contenedor_proyectos = document.querySelector("#portafolio .portafolio-proyectos");
+const $contenedor_modal = document.querySelector('#modal #modal_container');
+const $contenedor_contacto = document.querySelector("#contacto .contacto-contenido-informacion");
+
+(function cargar_datos(){
+    // Obtiene la informacion para las secciones del HTML desde una DB provisional (archivo JSON)
+    conocimientos = DATA.conocimientos;
+    servicios = DATA.servicios;
+    proyectos = DATA.proyectos;
+    contacto = DATA.contacto;
+
+    // Añada la informacion obtenida al HTML
+    agregar_datos_a_HTML(conocimientos, $contenedor_conocimientos)
+    agregar_datos_a_HTML(servicios, $contenedor_servicios);
+    agregar_datos_a_HTML(proyectos, $contenedor_proyectos);
+    agregar_datos_a_HTML(contacto, $contenedor_contacto);
+})();
+
+
+function agregar_datos_a_HTML(data, $contenedor) {
+    // Crea un plantilla HTML y la inserta en su contenedor
+    let plantilla_HTML = crear_plantilla_HTML(data, $contenedor);
+    $contenedor.innerHTML = plantilla_HTML;
+
+    // Agrega un evento a cada uno de los proyectos del $contenedor_proyectos
+    if ($contenedor == $contenedor_proyectos) {
+        Array.from($contenedor.getElementsByClassName('portafolio-proyecto')).map(($proyecto, indice) => {
+            $proyecto.addEventListener("click", () => abrir_modal(data[indice]))
+        });
+    }
+}
+
+function crear_plantilla_HTML(data, $contenedor) {
+    // String con formato HTML que sera devuelto
+    var plantilla_HTML = "";
+
+    // Selecciona la plantilla correspondiente para la informacion segun su contenedor
+    switch ($contenedor) {
+        // Plantilla para c/u de los conocimientos
+        case $contenedor_conocimientos:
+            data.map(conocimiento => {
+                plantilla_HTML += `
+                <p class="profesional-conocimiento-nombre">${conocimiento.titulo}</p>
+                <div class="profesional-conocimiento-nivel">
+                    <div class="profesional-nivel-barra n${conocimiento.nivel}">
+                        <div class="profesional-barra-relleno"></div>
+                    </div>
+                    <p class="profesional-nivel-porcentaje">${conocimiento.nivel}%</p>
+                </div>`
+            });
+            return plantilla_HTML;
+            
+        // Plantilla para c/u de los servicios (tipo A y B aplicados de manera intercalada)
+        case $contenedor_servicios:
+            data.map((servicio, indice) => {
+                if (indice % 2 == 0) {
+                    plantilla_HTML += `
+                    <div class="trabajo-servicio">
+                        <figure class="trabajo-contenedor-imagen">
+                            <img class="trabajo-imagen" 
+                                src="${servicio.imagen.url2x || servicio.imagen.url}" 
+                                srcset="${servicio.imagen.url2x ? servicio.imagen.url + ', ' + servicio.imagen.url2x + ' 2x' : ''}"
+                                alt="Pagina Web">
+                            <div class="trabajo-imagen-capa">
+                                <p class="trabajo-etiqueta">${servicio.fecha_inicio} - ${new Date().getFullYear()}</p>
+                                ${servicio.herramientas.includes('html') ? '<i class="trabajo-icono fab fa-html5"></i>' : ''}
+                                ${servicio.herramientas.includes('css') ? '<i class="trabajo-icono fab fa-css3-alt"></i>' : ''}
+                                ${servicio.herramientas.includes('javascript') ? '<i class="trabajo-icono fab fa-js-square"></i>' : ''}
+                                ${servicio.herramientas.includes('bootstrap') ? '<i class="trabajo-icono fab fa-bootstrap"></i>' : ''}
+                                ${servicio.herramientas.includes('git') ? '<i class="trabajo-icono fab fa-git-square"></i>' : ''}
+                                ${servicio.herramientas.includes('github') ? '<i class="trabajo-icono fab fa-github"></i>' : ''}
+                            </div>
+                        </figure>
+                        <div class="trabajo-descripcion">
+                            <h3 class="trabajo-descripcion-titulo">${servicio.titulo}</h3>
+                            <p class="trabajo-descripcion-parrafo">${servicio.descripcion}</p>
+                        </div>
+                    </div>`
+                }else{
+                    plantilla_HTML += `
+                    <div class="trabajo-servicio">
+                        <div class="trabajo-descripcion impar">
+                            <h3 class="trabajo-descripcion-titulo">${servicio.titulo}</h3>
+                            <p class="trabajo-descripcion-parrafo">${servicio.descripcion}</p>
+                        </div>
+                        <figure class="trabajo-contenedor-imagen impar">
+                            <div class="trabajo-imagen-capa">
+                                <p class="trabajo-etiqueta impar">${servicio.fecha_inicio} - ${new Date().getFullYear()}</p>
+                                ${servicio.herramientas.includes('html') ? '<i class="trabajo-icono fab fa-html5"></i>' : ''}
+                                ${servicio.herramientas.includes('css') ? '<i class="trabajo-icono fab fa-css3-alt"></i>' : ''}
+                                ${servicio.herramientas.includes('javascript') ? '<i class="trabajo-icono fab fa-js-square"></i>' : ''}
+                                ${servicio.herramientas.includes('bootstrap') ? '<i class="trabajo-icono fab fa-bootstrap"></i>' : ''}
+                                ${servicio.herramientas.includes('git') ? '<i class="trabajo-icono fab fa-git-square"></i>' : ''}
+                                ${servicio.herramientas.includes('github') ? '<i class="trabajo-icono fab fa-github"></i>' : ''}
+                            </div>
+                            <img class="trabajo-imagen" 
+                                src="${servicio.imagen.url2x || servicio.imagen.url}" 
+                                srcset="${servicio.imagen.url2x ? servicio.imagen.url + ', ' + servicio.imagen.url2x + ' 2x' : ''}"
+                                alt="Pagina Web">
+                        </figure>
+                    </div>`
+                }
+            })
+            return plantilla_HTML;
+
+        // Plantilla para c/u de los proyectos
+        case $contenedor_proyectos:
+            data.map(proyecto => {
+                plantilla_HTML += `
+                <figure class="portafolio-proyecto">
+                    <picture class="portafolio-imagen">
+                        <source srcset="${proyecto.imagen.portada.responsive}" media="(max-width: 600px)">
+                        <img class="portafolio-imagen" 
+                            src="${proyecto.imagen.portada.url2x || proyecto.imagen.portada.url}" 
+                            srcset="${proyecto.imagen.portada.url2x ? proyecto.imagen.portada.url + ', ' + proyecto.imagen.portada.url2x + ' 2x' : ''}"
+                            alt="Pagina Web">
+                    </picture>
+                    <div class="portafolio-descripcion">
+                        <h4 class="portafolio-descripcion-titulo">${proyecto.titulo}</h4>
+                        <p class="portafolio-descripcion-parrafo">${proyecto.resumen}</p>
+                        <i class="portafolio-icono fas fa-search-plus"></i>
+                    </div>
+                </figure>`
+            });
+            return plantilla_HTML;
+
+        // Plantilla para el contenido del modal
+        case $contenedor_modal:
+            var proyecto = data;
+            plantilla_HTML += `
+            <div class="modal-container-header">
+                <h3 class="modal-encabezado-titulo">${proyecto.titulo}</h3>
+                <button class="modal-encabezado-button" id="btn_cerrar_modal">×</button>
+            </div>
+            <div class="modal-container-body">
+                <figure class="modal-body-imagen">
+                    <img src="${proyecto.imagen.descripcion.url2x || proyecto.imagen.descripcion.url}" 
+                        srcset="${proyecto.imagen.descripcion.url2x ? proyecto.imagen.descripcion.url + ', ' + proyecto.imagen.descripcion.url2x + ' 2x' : ''}"
+                        alt="Pagina Web">
+                </figure>
+                <div class="modal-body-contenido">
+                    <h4 class="modal-contenido-titulo">Descripcion</h4>
+                    <p class="modal-contenido-parrafo">${proyecto.descripcion}</p>
+                    <h4 class="modal-contenido-titulo">URL ${proyecto.url.github ? '/ Github' : ''}</h4>
+                    <div class="modal-contenido-enlaces">
+                        <a class="modal-enlaces-url" href="${proyecto.url.website}" target="_blank">${proyecto.url.website}</a>
+                        ${proyecto.url.github ? 
+                            `<a class="modal-enlaces-github" href="${proyecto.url.github}" target="_blank"><i class="fab fa-github"></i></a>` 
+                        : ''}
+                    </div>
+                    <h4 class="modal-contenido-titulo">Requerimientos</h4>
+                    <div class="modal-contenido-requerimientos">
+                        ${String(proyecto.herramientas.map(herramienta => 
+                            `<span class="${herramienta}">${herramienta}</span>`
+                        )).replace(/,/g, ' ')}
+                    </div>
+                </div>
+            </div>`
+            return plantilla_HTML;
+
+        // Plantilla para el contenido de la seccion contacto
+        case $contenedor_contacto:
+            var contacto = data;
+            plantilla_HTML += `
+            <h2 class="contacto-informacion-titulo">Contacto</h2>
+            <p class="contacto-informacion-descripcion">${contacto.acerca_de_mi}</p>
+            <a class="contacto-informacion-correo" href="mailto:${contacto.correo}">${contacto.correo}</a>
+            <div class="contacto-informacion-redes-sociales">
+                ${contacto.redes_sociales.github ? 
+                    `<a href="${contacto.redes_sociales.github}" class="contacto-icono" target="_blank"><i class="fab fa-github"></i></a>`
+                : ''}
+                ${contacto.redes_sociales.instagram ? 
+                    `<a href="${contacto.redes_sociales.instagram}" class="contacto-icono" target="_blank"><i class="fab fa-instagram"></i></a>`
+                : ''}
+                ${contacto.redes_sociales.twitter ? 
+                    `<a href="${contacto.redes_sociales.twitter}" class="contacto-icono" target="_blank"><i class="fab fa-twitter"></i></a>`
+                : ''}
+                ${contacto.redes_sociales.linkedin ? 
+                    `<a href="${contacto.redes_sociales.linkedin}" class="contacto-icono" target="_blank"><i class="fab fa-linkedin-in"></i></a>`
+                : ''}
+            </div>`
+            return plantilla_HTML;
+    }
+}
+
 /* ----------------------------------------------------------------- */
 /* TRABAJAMOS CON EL MENU DESPLEGABLE EN VERSION RESPONSIVE (<600PX) */
 /* ----------------------------------------------------------------- */
@@ -23,116 +214,42 @@ function monstrarAlternativamenteMenu() {
     $menu.style.bottom = 0;
     // Bloqueamos el desplazamiente en la web cuando esta activo el menu
     document.body.classList.toggle('overflow-hidden');
-    
 }
 
 /* ------------------------------------------------------------- */
 /* TRABAJAMOS CON EL MODAL QUE MOSTRARA LOS DATOS DE UN PROYECTO */
 /* ------------------------------------------------------------- */
 
-// Plantillas para c/u de los proyectos mediante las cuales se creara un modal personalizado
-const plantillaPlatziConf = {
-    titulo: 'Platzi Conf Hawaii',
-    imagen: './assets/img/public/modal-platzi-conf.png',
-    imagen2x: './assets/img/public/modal-platzi-conf@2x.png',
-    descripcion: 'Platzi Conf Hawaii fue desarrolado como modelo para promover un evento social. Cuenta con una interface responsive de diseño atractivo lograda con ayuda de Bootstrap.',
-    url: {
-        website: 'luisvalladaresc.github.io/platzi-conf',
-        github: 'https://github.com/LuisValladaresC/platzi-conf'
-    },
-    requerimientos: ['html', 'css', 'bootstrap']
-}
-const plantillaPlatziVideo = {
-    titulo: 'Platzi Video',
-    imagen: './assets/img/public/modal-platzi-video.png',
-    imagen2x: './assets/img/public/modal-platzi-video@2x.png',
-    descripcion: 'Platzi Video fue creado como modelo para un servicio de películas online. Cuenta con un código que realiza multiples peticiones a API\'s externas y una interface responsive de diseño simple y pulido.',
-    url: {
-        website: 'luisvalladaresc.github.io/platzi-video',
-        github: 'https://github.com/LuisValladaresC/platzi-video'
-    },
-    requerimientos: ['html', 'css', 'javascript']
-}
-const plantillaPortafolio = {
-    titulo: 'Portafolio Personal',
-    imagen: './assets/img/public/modal-portafolio.png',
-    imagen2x: './assets/img/public/modal-portafolio@2x.png',
-    descripcion: 'Sitio web actual desarrollado para promover mi marca personal. Cuenta con un sistema de mensajes de correo, multiples y muy bien pulidas animaciones y una interface responsive que resalta por su diseño simple, calido y atractivo.',
-    url: {
-        website: 'www.luisvc.com',
-    },
-    requerimientos: ['html', 'css', 'javascript']
-}
-
-// Agregamos un escucha a c/u de los proyectos del portafolio, el cual, creara el modal mediante las plantillas
-const $platziConf = document.getElementById("platzi_conf");
-const $platziVideo = document.getElementById("platzi_video");
-const $portafolioPersonal = document.getElementById("portafolio_personal");
-
-$platziConf.addEventListener('click', () => crearPlantillaHtmlModal(plantillaPlatziConf));
-$platziVideo.addEventListener('click', () => crearPlantillaHtmlModal(plantillaPlatziVideo));
-$portafolioPersonal.addEventListener('click', () => crearPlantillaHtmlModal(plantillaPortafolio));
-
-// Esta funcion crea un elemento HTML con los datos del proyecto y lo añade al modal 
-function crearPlantillaHtmlModal(plantilla) {
-    let $modalContainer = document.createElement('div');
-    $modalContainer.setAttribute('id', 'modal_container');
-
-    $modalContainer.innerHTML = 
-    `<div class="modal-container-header">
-        <h3 class="modal-encabezado-titulo">${plantilla.titulo}</h3>
-        <button class="modal-encabezado-button" id="btn_cerrar_modal">×</button>
-    </div>
-    <div class="modal-container-body">
-        <figure class="modal-body-imagen">
-            <img src="${plantilla.imagen2x}" srcset="${plantilla.imagen}, ${plantilla.imagen2x} 2x" alt="${plantilla.titulo}">
-        </figure>
-        <div class="modal-body-contenido">
-            <h4 class="modal-contenido-titulo">Descripcion</h4>
-            <p class="modal-contenido-parrafo">${plantilla.descripcion}</p>
-            <h4 class="modal-contenido-titulo">URL ${plantilla.url.github ? '/ Github' : ''}</h4>
-            <div class="modal-contenido-enlaces">
-                <a class="modal-enlaces-url" href="https://${plantilla.url.website}" target="_blank">${plantilla.url.website}</a>
-                ${plantilla.url.github ? `<a class="modal-enlaces-github" href="${plantilla.url.github}" target="_blank"><i class="fab fa-github"></i></a>` : ''}
-            </div>
-            <h4 class="modal-contenido-titulo">Requerimientos</h4>
-            <div class="modal-contenido-requerimientos">
-                ${String(plantilla.requerimientos.map(lenguaje => `<span class="${lenguaje}">${lenguaje}</span>`)).replace(/,/g, ' ')}
-            </div>
-        </div>
-    </div>`
-
-    $modal.appendChild($modalContainer);
-    $modalContainer.addEventListener('click', event => event.stopPropagation());
-    document.getElementById("btn_cerrar_modal").addEventListener('click', () => cerrarModal(event));
-    
-    abrirModal();
-}
-
-// Agregamos un escucha al modal que llamara una funcion que lo cerrara
 const $modal = document.getElementById('modal');
-$modal.addEventListener('click', () => cerrarModal(event));
 
-function abrirModal() {
-    // Agrega la clase active y de manera asicrona sin demora la clase inactive para activar una transicion
+function abrir_modal(data){
+    // Agrega la informacion del proyecto al contenido del modal
+    agregar_datos_a_HTML(data, $contenedor_modal)
+
+    // Agrega la clase active y de manera asicrona remueve la clase inactive para activar su transicion
     $modal.classList.add('active');
     setTimeout(() => $modal.classList.remove('inactive'), 0);
-    // Bloqueamos el desplazamiente en la web
+
+    // Bloquea el desplazamiente en la web
     document.body.classList.add('overflow-hidden');
+
+    // Agrega un evento con la funcion cerrar_modal al boton cerrar del modal
+    document.getElementById("btn_cerrar_modal").addEventListener('click', () => cerrar_modal(event));
 }
 
-function cerrarModal(event) {
-    // Evita que un elemento padre reaccione al evento con el mismo click
-    event.stopPropagation()
+// Agrega un evento para cerrar el modal dando clic afuera de su contenido
+$modal.addEventListener('click', () => cerrar_modal());
+$contenedor_modal.addEventListener('click', e => e.stopPropagation());
+
+function cerrar_modal() {
     // Agrega la clase inactive que define una animacion al elemento, a la cual, agregamos un escucha
     $modal.classList.add('inactive');
-    $modal.addEventListener('animationend', cerrarModal)
+    $modal.addEventListener('animationend', concluir_cierre)
 
     // funcion del evento que remueve la clase active, el #modal_container y su escucha al finalizar la animacion
-    function cerrarModal() {
+    function concluir_cierre() {
         $modal.classList.remove('active');
-        $modal.removeEventListener('animationend', cerrarModal)
-        $modal.children[0].remove()
+        $modal.removeEventListener('animationend', concluir_cierre)
         // Desbloqueamos el desplazamiente en la web
         document.body.classList.remove('overflow-hidden');
     }
@@ -143,8 +260,8 @@ function cerrarModal(event) {
 /* -------------------------------------------------------------------------------------- */
 
 // Arreglos de elementos a los que definiremos una animacion
-const $barrasNivel = Array.from(document.getElementsByClassName('barra-relleno'))
-const $porcentajesNivel = Array.from(document.getElementsByClassName('tema-porcentaje'))
+const $barrasNivel = Array.from(document.getElementsByClassName('profesional-barra-relleno'))
+const $porcentajesNivel = Array.from(document.getElementsByClassName('profesional-nivel-porcentaje'))
 // Las animaciones se haran 1 despues de otra mediante este retardo
 var retardoAnimacion = 0;
 
