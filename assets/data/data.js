8717ad1a31d60c4f5c011a2bd2c868edd194bb42
Luis Valladares <ls.valladares7@gmail.com>
2022-01-14 15:14:07 -0600

    Base de datos actualizada. Se han modificados los proyectos recientes.

--- assets/data/data.js
+++ assets/data/data.js
@@ -62,139 +62,122 @@ const DATA = {
         },
     ],
     proyectos: [
-        /* FORMATOS DE IMAGEN = portada (cuadrada) | portada.responsive (panoramica 10:6) | descripcion = (mockup 7:5) */
+        /* FORMATOS DE IMAGEN = portada (ancho-maximo: 553px - 16:9) | descripcion (ancho-maximo: 967px - 16:9) */
         {
-            titulo: "Portafolio Personal",
-            resumen: "Pagina web actual creada para promover mi marca personal.",
-            descripcion: "Pagina web actual creada para promover mi marca personal. Cuenta con un diseño simple, elegante, y muy bien pulido desarrollado desde 0 utilizando HTML5, CSS3 y JavaScript nativo. Su interfaz es completamente responsiva, pues fue programada para adaptarse de manera fluida en cualquier tipo de pantalla, además es totalmente interactiva, con carga dinámica de contenido, múltiples animaciones y optimizada para un máximo rendimiento y velocidad de carga, lo que en conjunto garantiza de una experiencia cálida y memorable.", 
+            titulo: "Giacchetta Networks",
+            resumen: "",
+            descripcion: "Proyecto en el que se desarrollo la pagina web de Giacchetta Networks, empresa que brinda servicios relacionados con Marketing, Negocios y Desarrollo de Proyectos. El desarrollado comenzó de 0, incluido su diseño, y se llevo a cabo con ayuda de \"Front\"' una plantilla de Bootstrap. Cuenta con múltiples interfaces, desde su pagina de inicio, un blog e incluso términos y condiciones de uso. Cabe mencionar, que cada interfaz es responsiva e interactiva, programada para adaptarse a cualquier tipo de pantalla, con posibilidad de incluir diferentes idiomas, carga dinámica de contenido y optimizada para tener un excelente rendimiento y velocidad de carga.", 
             url: {
-                website: 'https://luisvalladaresc.github.io/portafolio',
-                github: ''
+                website: 'https://luisvalladaresc.github.io/',
             },
             imagen: {
                 portada: {
-                    url: "./assets/img/public/proyectos/portafolio-personal.png", // (1005px*1005px)
-                    url2x: "./assets/img/public/proyectos/portafolio-personal@2x.png",
-                    responsive: "./assets/img/public/proyectos/portafolio-personal-stretch.png", // (1200px*720px)
-                    responsive2x: "./assets/img/public/proyectos/portafolio-personal-stretch@2x.png"
+                    url: "./assets/img/public/proyectos/giacchetta_networks.png",
+                    url2x: "",
                 },
                 descripcion: {
-                    url: "./assets/img/public/proyectos/portafolio-personal-full.png",
-                    url2x: "./assets/img/public/proyectos/portafolio-personal-full@2x.png"
+                    url: "./assets/img/public/proyectos/giacchetta_networks_description.png",
+                    url2x: "",
                 },
             },
-            herramientas: ["html", "css", "javascript"]
+            herramientas: ["html", "css", "javascript", "bootstrap", "nunjucks", "gulp", "git", "gitlab"]
         },
         {
-            titulo: "Rafa Cavestany",
-            resumen: "Página web de diseño minimalista creada para promover una marca personal.",
-            descripcion: "Página web de diseño minimalista creada para promover una marca personal. Este proyecto fue desarrollado completamente a partir de un diseño proporcionado, utilizando HTML5, CSS3 y JavaScript nativo. Cuenta con una interfaz responsiva que fue programada para adaptarse de manera fluida en cualquier tipo de pantalla, totalmente interactiva, con múltiples animaciones, carga dinámica de contenido y optimizada para un máximo rendimiento y velocidad de carga.",
+            titulo: "Moegui",
+            resumen: "",
+            descripcion: "Proyecto en el que se desarrollo la pagina web de Moegui, empresa que brinda servicios de consultoría relacionados con DevOps, SecOps y TestOps. El desarrollado comenzó a partir de una pagina de inicio y se llevo a cabo con ayuda de \"Front\" una plantilla de Bootstrap. Cuenta con multiples interfaces, como su pagina de inicio (actualizada), nuevas paginas como acerca de, contacto, blog, documentación, ayuda, políticas de privacidad, términos y condiciones, e incluso paginas creadas dinámicamente para cada uno de sus productos, blogs, servicios y oportunidades de trabajo. Cabe mencionar, que cada interfaz es responsiva e interactiva, programada para adaptarse a cualquier tipo de pantalla, con posibilidad de incluir diferentes idiomas, con múltiples animaciones, carga dinámica de contenido y optimizada para tener un excelente rendimiento y velocidad de carga.", 
+            url: {
+                website: 'https://luisvalladaresc.github.io/',
+            },
+            imagen: {
+                portada: {
+                    url: "./assets/img/public/proyectos/moegui.png",
+                    url2x: "",
+                },
+                descripcion: {
+                    url: "./assets/img/public/proyectos/moegui_description.png",
+                    url2x: "",
+                },
+            },
+            herramientas: ["html", "css", "javascript", "bootstrap", "nunjucks", "gulp", "git", "gitlab"]
+        },
+        {
+            titulo: "Portafolio",
+            resumen: "",
+            descripcion: "Proyecto en el que se desarrollo la pagina web actual enfocada en promover mi trabajo. El desarrollado comenzó de 0, incluido su diseño. Cuenta con una interfaz responsiva e interactiva, programada para adaptarse a cualquier tipo de pantalla, con animaciones sutiles, carga dinámica de contenido y optimizada para tener un excelente rendimiento y velocidad de carga.", 
+            url: {
+                website: 'https://luisvalladaresc.github.io/',
+                github: 'https://github.com/LuisValladaresC/luisvalladaresc.github.io'
+            },
+            imagen: {
+                portada: {
+                    url: "./assets/img/public/proyectos/portafolio.png",
+                    url2x: "",
+                },
+                descripcion: {
+                    url: "./assets/img/public/proyectos/portafolio_description.png",
+                    url2x: "",
+                },
+            },
+            herramientas: ["html", "css", "javascript", "git", "github"]
+        },
+        {
+            titulo: "Sistema de Inventarios",
+            resumen: "",
+            descripcion: "Proyecto en el que se dio mantenimiento y desarrollaron nuevas características para una aplicación web enfocada gestionar inventarios de productos y realizar pedidos. Las nuevas implementaciones fueron desde cambios menores en las diferentes interfaces, revisión y reparación de errores, diseño y desarrollo de nuevas interfaces con su lógica de base de datos e interactividad con el servidor.", 
+            url: {
+                website: 'https://luisvalladaresc.github.io/',
+            },
+            imagen: {
+                portada: {
+                    url: "./assets/img/public/proyectos/sistema_inventarios.png",
+                    url2x: "",
+                },
+                descripcion: {
+                    url: "./assets/img/public/proyectos/sistema_inventarios_description.png",
+                    url2x: "",
+                },
+            },
+            herramientas: ["php", "html", "css", "javascript", "bootstrap", "mysql"]
+        },
+        {
+            titulo: "RCavestany",
+            resumen: "",
+            descripcion: "Proyecto en el que se desarrollo una pagina web minimalista enfocada en promover la marca personal del cliente. El desarrollado comenzó a partir de un diseño inicial. Cuenta con una interfaz responsiva e interactiva, programada para adaptarse a cualquier tipo de pantalla, con animaciones sutiles, carga dinámica de contenido y optimizada para tener un excelente rendimiento y velocidad de carga.",
             url: {
                 website: 'https://luisvalladaresc.github.io/Rafa-Cavestany/',
-                github: ''
             },
             imagen: {
                 portada: {
-                    url: "./assets/img/public/proyectos/rafa-ravestany.png", // (1000px*1000px)
-                    url2x: "./assets/img/public/proyectos/rafa-ravestany@2x.png",
-                    responsive: "./assets/img/public/proyectos/rafa-ravestany-stretch.png", // (1800px*1080px)
-                    responsive2x: "./assets/img/public/proyectos/rafa-ravestany-stretch@2x.png"
+                    url: "./assets/img/public/proyectos/rcavestany.png",
+                    url2x: "",
                 },
                 descripcion: {
-                    url: "./assets/img/public/proyectos/rafa-ravestany-full.jpg",
-                    url2x: "./assets/img/public/proyectos/rafa-ravestany-full@2x.jpg"
+                    url: "./assets/img/public/proyectos/rcavestany_description.png",
+                    url2x: ""
                 },
             },
             herramientas: ["html", "css", "javascript"]
         },
         {
-            titulo: "Kutenda",
-            resumen: "Interfaz web creada como fachada de una plataforma de préstamos en linea.",
-            descripcion: "Interfaz web creada como fachada de una plataforma de préstamos en línea. Este proyecto fue maquetado completamente a partir de un diseño proporcionado, utilizando HTML5, CSS3 y Bootstrap. Su interfaz es totalmente responsiva programada para adaptarse de manera fluida en casi cualquier tipo de pantalla, además esta optimizada para un máximo rendimiento y velocidad de carga.",
+            titulo: "XabiDB",
+            resumen: "",
+            descripcion: "Proyecto en el que se desarrollo una pagina web enfocada en promover la marca personal del cliente. El desarrollado comenzó a partir de un diseño inicial. Cuenta con una interfaz responsiva e interactiva, programada para adaptarse a cualquier tipo de pantalla, con animaciones sutiles y optimizada para tener un excelente rendimiento y velocidad de carga.",
             url: {
-                website: 'https://luisvalladaresc.github.io/kutenda/',
-                github: ''
+                website: 'https://luisvalladaresc.github.io/XabiDB/',
             },
             imagen: {
                 portada: {
-                    url: "./assets/img/public/proyectos/kutenda.png", // (1920px*1920px)
-                    url2x: "./assets/img/public/proyectos/kutenda@2x.png",
-                    responsive: "./assets/img/public/proyectos/kutenda-stretch.png", // (1920px*1170px)
-                    responsive2x: "./assets/img/public/proyectos/kutenda-stretch@2x.png"
+                    url: "./assets/img/public/proyectos/xabidb.png",
+                    url2x: "",
                 },
                 descripcion: {
-                    url: "./assets/img/public/proyectos/kutenda-full.png",
-                    url2x: "./assets/img/public/proyectos/kutenda-full@2x.png"
-                },
-            },
-            herramientas: ["html", "css", "bootstrap"]
-        },
-        {
-            titulo: "Platzi Video",
-            resumen: "Pagina web creada como modelo para un servicio de películas en línea.",
-            descripcion: "Pagina web creada como modelo para un servicio de películas en línea. Este proyecto fue desarrollado desde 0 utilizando HTML5, CSS3 y JavaScript nativo, cuenta con una interfaz de diseño simple y muy bien pulido, totalmente interactiva, con carga dinámica de contenido y una adaptación fluida en cualquier tipo de pantalla.",
-            url: {
-                website: 'https://luisvalladaresc.github.io/platzi-video',
-                github: 'https://github.com/LuisValladaresC/platzi-video'
-            },
-            imagen: {
-                portada: {
-                    url: "./assets/img/public/proyectos/platzi-video.png", // (1000px*1000px)
-                    url2x: "./assets/img/public/proyectos/platzi-video@2x.png",
-                    responsive: "./assets/img/public/proyectos/platzi-video-stretch.png", // (1600px*960px)
-                    responsive2x: "./assets/img/public/proyectos/platzi-video-stretch@2x.png"
-                },
-                descripcion: {
-                    url: "./assets/img/public/proyectos/platzi-video-full.png",
-                    url2x: "./assets/img/public/proyectos/platzi-video-full@2x.png"
+                    url: "./assets/img/public/proyectos/xabidb_description.png",
+                    url2x: ""
                 },
             },
             herramientas: ["html", "css", "javascript"]
         },
-        {
-            titulo: "Platzi Conf Hawaii",
-            resumen: "Pagina web creada como modelo para promover un evento social.",
-            descripcion: "Pagina web creada como modelo para promover un evento social. Este proyecto fue desarrollado desde 0 utilizando HTML5, CSS3 y Bootstrap, el cual cuenta con una interfaz responsiva y totalmente interactiva.",
-            url: {
-                website: 'https://luisvalladaresc.github.io/platzi-conf',
-                github: 'https://github.com/LuisValladaresC/platzi-conf'
-            },
-            imagen: {
-                portada: {
-                    url: "./assets/img/public/proyectos/platzi-conf.png", // (1100px*1100px)
-                    url2x: "./assets/img/public/proyectos/platzi-conf@2x.png",
-                    responsive: "./assets/img/public/proyectos/platzi-conf-stretch.png", // (1200px*720px)
-                    responsive2x: "./assets/img/public/proyectos/platzi-conf-stretch@2x.png"
-                },
-                descripcion: {
-                    url: "./assets/img/public/proyectos/platzi-conf-full.png",
-                    url2x: "./assets/img/public/proyectos/platzi-conf-full@2x.png"
-                },
-            },
-            herramientas: ["html", "css", "bootstrap"]
-        },
-        {
-            titulo: "Portafolio Demo",
-            resumen: "Pagina web creada como modelo para un sitio web personal.",
-            descripcion: "Pagina web creada como modelo para un sitio web personal. Este proyecto fue desarrollado desde 0 utilizando HTML5 y CSS3. Cuenta con una interfaz responsiva programada para una adaptación fluida en casi cualquier tipo de pantalla.",
-            url: {
-                website: 'https://luisvalladaresc.github.io/portafolio-ejemplo/',
-                github: 'https://github.com/LuisValladaresC/portafolio-ejemplo'
-            },
-            imagen: {
-                portada: {
-                    url: "./assets/img/public/proyectos/leonidas-esteban.png", // (1024px*1024px)
-                    url2x: "./assets/img/public/proyectos/leonidas-esteban@2x.png",
-                    responsive: "./assets/img/public/proyectos/leonidas-esteban-stretch.png", // (1020px*612px)
-                    responsive2x: "./assets/img/public/proyectos/leonidas-esteban-stretch@2x.png"
-                },
-                descripcion: {
-                    url: "./assets/img/public/proyectos/leonidas-esteban-full.png",
-                    url2x: "./assets/img/public/proyectos/leonidas-esteban-full@2x.png"
-                },
-            },
-            herramientas: ["html", "css"]
-        }
     ],
     contacto: {
         acerca_de_mi: "Soy un desarrollador de páginas web con mucha experiencia y un alto nivel de estudios en la rama. Justo ahora me encuentro explorando nuevas oportunidades laborales. Así que si lo desea puede escribirme ahora mismo a alguna de mis redes sociales para que juntos pongamos en marcha tu proyecto.",
