15d1169d5156dadee58b0b71cf82fcbd33b3d145
Luis Valladares <ls.valladares7@gmail.com>
2024-02-12 15:22:57 -0600

    feat: actualizacion menor en textos de la pagina

--- src/lang/es.js
+++ src/lang/es.js
@@ -42,7 +42,7 @@ export default {
       line3: 'Valladares',
     },
     subtitle: 'Desarrollador Web',
-    about_me: 'Como Desarrollador Web me especializo en el area denominada como "Frontend", es decir, en todo lo relacionado con el desarrollo de una interfaz web y su interactividad con los usuarios. Cuento con mas de 5 años de experiencia y puedo resaltar mi trabajo como Freelancer, mediante el cual, he tenido la oportunidad de trabajar de manera remota y colaborativa con personas y equipos de trabajo de diferentes partes del mundo. Mi objetivo actual es seguir mejorando mis habilidades profesionales, por lo que me encuentro en la actualización continua de mis conocimientos y en la búsqueda de nuevos retos profesionales.',
+    about_me: 'Como Desarrollador Web me especializo en el area denominada como Frontend, es decir, en todo lo relacionado con el desarrollo de una interfaz web y su interactividad con los usuarios. Cuento con mas de 5 años de experiencia y puedo resaltar mi trabajo como Freelancer, mediante el cual, he tenido la oportunidad de trabajar de manera remota y colaborativa con personas y equipos de trabajo de diferentes partes del mundo. Mi objetivo actual es seguir mejorando mis habilidades profesionales, por lo que me encuentro en la actualización continua de mis conocimientos y en la búsqueda de nuevos retos profesionales.',
     curriculum: {
       text: 'Descargar Currículum',
       link: 'https://drive.google.com/file/d/1E6vjqJgxoJl3Y-NZ9BIkfEoY7s0yirfn/view'
@@ -142,7 +142,7 @@ export default {
     projects: [
       {
         title: 'Portafolio',
-        description: 'Página web actual enfocada en promover mi marca personal, en donde muestro información acerca de mí, mi perfil profesional, mi trabajo y un portafolio con algunos de los proyectos realizado previamente. Su interfaz cuenta con un diseño programado para adaptarse a cualquier tamaño de pantalla, optimizada para tener un excelente rendimiento y con animaciones sutiles que hacen más ameno su uso. Inicialmente, se programó con HTML, CSS y JavaScript vanilla, luego se implementó HandlebarsJS como gestor de plantillas, y finalmente, en su versión #3, se actualizó a la estructura de componentes de React. El proyecto se desarrolló desde 0, incluido su diseño, mi rol ha sido tanto Frontend Developer como Diseñador, y hasta la actualidad, soy la única persona involucrada en el desarrollo.',
+        description: 'Página web enfocada en promover mi marca personal, en donde encontrará información acerca de mí, mi perfil profesional y un portafolio con algunos de mis proyectos. Su interfaz cuenta con un diseño programado para adaptarse a cualquier tamaño de pantalla, optimizada para tener un excelente rendimiento y con animaciones sutiles que hacen más ameno su uso. Inicialmente, el proyecto fue desarrollado con HTML, CSS y JavaScript vanilla, posteriormente, se implementó HandlebarsJS como gestor de plantillas, y finalmente, en su versión #3, se actualizó a la estructura de componentes de React. Empecé a trabajar desde 0, incluido el diseño del proyecto. Mi rol ha sido tanto Frontend Developer como Diseñador, y hasta la actualidad, soy la única persona involucrada en el desarrollo.',
         website: 'https://luisvalladaresc.github.io',
         github: 'https://github.com/LuisValladaresC/luisvalladaresc.github.io',
         image: {
@@ -162,7 +162,7 @@ export default {
       },
       {
         title: 'CCoins',
-        description: 'CCoins es un Mercado P2P de Criptomonedas, servicio que permite a sus usuarios comprar, vender e intercambiar criptomonedas. Su página web cuenta con muchísimas interfaces, un diseño responsivo programado para adaptarse a cualquier tamaño de pantalla, carga dinámica de contenido y mucha interactividad. Fue desarrollada en el entorno de Ruby on Rails, utilizando el gestor de plantillas Stimulus, Vue y TailwindCSS para la maquetación web, además de JavaScript para el manejo de la interactividad del lado del cliente. Empecé a trabajar desde una etapa media hasta una etapa avanzada de su desarrollo con un equipo de aproximadamente 8 personas. Mi rol fue como Frontend Developer, sin embargo, también colabore en el área de diseño y backend.',
+        description: 'CCoins es un Mercado P2P de Criptomonedas, servicio que permite a sus usuarios comprar, vender e intercambiar criptomonedas. Su aplicación web cuenta con muchísimas interfaces, un diseño responsivo programado para adaptarse a cualquier tamaño de pantalla y carga dinámica de contenido. Fue desarrollada en el entorno de Ruby on Rails, utilizando el gestor de plantillas Stimulus, Vue y TailwindCSS para la maquetación web, además de JavaScript para el manejo de la interactividad del lado del cliente. Empecé a trabajar desde una etapa media hasta una etapa avanzada del proyecto. Mi rol fue como Frontend Developer dentro de un equipo de aproximadamente 8 personas, pero también colaboré en el área de diseño y backend.',
         website: 'https://ccoins.io',
         image: {
           desktop: {
@@ -175,11 +175,11 @@ export default {
           },
           background_color: '#499789',
         },
-        tools: ['html', 'css', 'javascript', 'vue', 'tailwindcss', 'ruby', 'postgresql', 'git', 'github']
+        tools: ['html', 'css', 'javascript', 'vue', 'tailwindcss', 'ruby', 'rails', 'postgresql', 'git', 'github']
       },
       {
         title: 'Giacchetta Networks',
-        description: 'Giacchetta Networks es una empresa que brinda servicios relacionados con Marketing, Negocios y Desarrollo de Proyectos. Su página web fue creada para proporcionar información acerca de su trabajo, conectar con nuevos clientes y como un blog. Cuenta con múltiples interfaces, un diseño responsivo programado para adaptarse a cualquier tamaño de pantalla, posibilidad de incluir múltiples idiomas, carga dinámica de contenido y optimizada para tener un excelente rendimiento. El proyecto se realizó con ayuda de "Front" una plantilla premium de Bootstrap, sin embargo, durante su desarrollo incorporé múltiples cambios en el framework, como la inclusión de Nunjucks como gestor de plantillas y la compilación para múltiples idiomas. El proyecto se desarrolló desde 0, mi rol fue como Frontend Developer, aportando en gran medida en el área de diseño, y hasta finalizar un MVP de la página web, fui el único programador en el desarrollo.',
+        description: 'Giacchetta Networks es una empresa que brinda servicios relacionados con marketing, negocios y desarrollo de proyectos. Su página web fue creada para proporcionar información acerca de su trabajo, conectar con nuevos clientes y como un blog. Cuenta con múltiples interfaces, un diseño responsivo programado para adaptarse a cualquier tamaño de pantalla, posibilidad de incluir múltiples idiomas, carga dinámica de contenido y optimizada para tener un excelente rendimiento. El proyecto fue desarrollado con HTML, CSS, Javascript y Bootstrap, utilizando como framework "Front", una plantilla premium de Bootstrap, a la cual se incorporaron algunos cambios, como la inclusión de Nunjucks como gestor de plantillas y una base de datos que maneja los textos en la página web y sus posibles idiomas. Empecé a trabajar desde 0, mi rol fue como Frontend Developer, pero también colaboré en gran medida en el área de diseño y fui el único programador en el desarrollo.',
         website: 'https://luisvalladaresc.github.io/giacchetta-networks',
         image: {
           desktop: {
@@ -196,7 +196,7 @@ export default {
       },
       {
         title: 'Moegui',
-        description: 'Moegui es una empresa que brinda servicios de consultoría relacionados con DevOps, SecOps y TestOps. Su página web fue creada para proporcionar información acerca de su trabajo, productos y servicios, conectar con nuevos clientes y como un blog. Cuenta con más de 12 interfaces, un diseño responsivo programado para adaptarse a cualquier tamaño de pantalla, posibilidad de incluir múltiples idiomas, carga dinámica de contenido y optimizada para tener un excelente rendimiento. El proyecto se realizó con ayuda de "Front" una plantilla premium de Bootstrap, sin embargo, durante su desarrollo incorporé múltiples cambios en el framework, como la inclusión de Nunjucks como gestor de plantillas y la compilación para múltiples idiomas. Empecé a trabajar en una etapa muy inicial de su desarrollo, mi rol fue como Frontend Developer, aportando en gran medida en el área de diseño, y hasta finalizar un MVP de la página web, fui el único programador en el desarrollo.',
+        description: 'Moegui es una empresa que brinda servicios de consultoría relacionados con DevOps, SecOps y TestOps. Su página web fue creada para proporcionar información acerca de su trabajo, productos y servicios, conectar con nuevos clientes y como un blog. Cuenta con más de 12 interfaces, un diseño responsivo programado para adaptarse a cualquier tamaño de pantalla, posibilidad de incluir múltiples idiomas, carga dinámica de contenido y optimizada para tener un excelente rendimiento. El proyecto fue desarrollado con HTML, CSS, Javascript y Bootstrap, utilizando como framework "Front", una plantilla premium de Bootstrap, a la cual se incorporaron algunos cambios, como la inclusión de Nunjucks como gestor de plantillas y una base de datos que maneja los textos en la página web y sus posibles idiomas. Empecé a trabajar en una etapa muy inicial del proyecto, mi rol fue como Frontend Developer, pero también colaboré en gran medida en el área de diseño y fui el único programador en el desarrollo.',
         website: 'https://luisvalladaresc.github.io/moegui',
         image: {
           desktop: {
@@ -213,7 +213,7 @@ export default {
       },
       {
         title: 'Sistema de Inventarios',
-        description: 'Aplicación web diseñada para la gestión de un inventario de productos, desde gestionar compras, proveedores, productos, almacenes, zonas, categorías y otras funcionalidades como reportes personalizados. Durante el desarrollo se implementaron cambios menores en las interfaces existentes, revisión y reparación de errores tanto backend, frontend y de base de datos, y finalmente, diseño y desarrollo de nuevas interfaces y su correspondiente lógica backend y base de datos. Empecé a trabajar en una etapa media de su desarrollo, mi rol fue FullStack y hasta finalizar un MVP del proyecto fui la única persona en el desarrollo. Específicamente, durante ese proceso estuve encargado de trabajar en el Backend, para el cual se utilizaba PHP y Laravel, en el Frontend utilizando el gestor de plantillas Blade, HTML y CSS para la maquetación web, además de JavaScript para el manejo de la interactividad del lado del cliente, y finalmente, en su base de datos se utilizaba MySQL.',
+        description: 'Aplicación web diseñada para la gestión de un inventario de productos, desde administrar compras, proveedores, productos, almacenes, zonas, categorías y otras funcionalidades como reportes personalizados. El proyecto fue desarrollado en el entorno de PHP y Laravel, utilizando el gestor de plantillas Blade, HTML, CSS y Javascript para la maquetación web e interactividad del lado del cliente, y MySQL como base de datos. Empecé a trabajar en una etapa media de su desarrollo, mi rol fue FullStack y hasta finalizar un MVP del proyecto, fui la única persona en el desarrollo. Durante ese proceso, se implementaron cambios menores en las interfaces existentes, revisión y reparación de errores tanto backend, frontend y de base de datos, y finalmente, diseño y desarrollo de nuevas interfaces y su correspondiente lógica backend y de base de datos.',
         website: 'https://www.behance.net/gallery/135108777/Sistema-de-Inventarios',
         image: {
           desktop: [
@@ -228,11 +228,11 @@ export default {
           mobile: null,
           background_color: '#367fa9',
         },
-        tools: ['php', 'html', 'css', 'javascript', 'bootstrap', 'mysql']
+        tools: ['html', 'css', 'javascript', 'php', 'laravel', 'mysql', 'git', 'github']
       },
       {
         title: 'RCavestany',
-        description: 'Página web enfocada en promover la marca personal del cliente, en donde se proporciona información acerca de su persona, perfil profesional, redes sociales y métodos de contacto. Su interfaz posee un diseño minimalista y responsivo programado para adaptarse a cualquier tamaño de pantalla, con carga dinámica de contenido, animaciones sutiles muy bien incorporadas y optimizada para tener un excelente rendimiento. El proyecto se desarrolló desde 0, mi rol fue como Frontend Developer, y fui el único programador en el desarrollo.',
+        description: 'Página web enfocada en promover la marca personal del cliente, en donde se proporciona información acerca de su persona, perfil profesional, redes sociales y métodos de contacto. Su interfaz posee un diseño minimalista y responsivo programado para adaptarse a cualquier tamaño de pantalla, con animaciones sutiles muy bien incorporadas, carga dinámica de contenido y optimizada para tener un excelente rendimiento. El proyecto fue desarrollado con HTML, CSS y JavaScript. Empecé a trabajar a partir de un diseño inicial y fui el único programador en el desarrollo.',
         website: 'http://www.rcavestany.com',
         image: {
           desktop: {
@@ -245,7 +245,7 @@ export default {
           },
           background_color: '#f4f4f4',
         },
-        tools: ['html', 'css', 'javascript']
+        tools: ['html', 'css', 'javascript', 'git', 'github']
       },
     ],
     modal: {
@@ -258,8 +258,8 @@ export default {
   contact: {
     id: 'contacto',
     title: 'Contacto',
-    description: '¿Necesita ayuda con el desarrollo de una página web? Pues no dudes en escribirme con los detalles del trabajo que desea realizar y para mí será un placer brindarle una asesoría profesional para el proyecto.',
-    email: 'ls.valladares7@gmail.com',
+    description: '¿Necesita ayuda con el desarrollo de una página web? Pues no dude en escribirme con los detalles del trabajo que desea realizar, será un placer brindarle mis servicios.',
+    email: 'luis.valladares.dev@gmail.com',
     phone: '',
     form: {
       name: {
